f8597672933af133f400d80c3c2847569471daf2
Jorge E. Hernández <hdez.jeduardo@gmail.com>
2023-08-12 14:24:30 -0600

    Refactor is_message() function

--- utils/util.py
+++ utils/util.py
@@ -4,23 +4,28 @@ class Util:
 
     @staticmethod
     def is_message(request) -> bool:
-        json_data = json.loads(request.data)
-        if "entry" in json_data:
-            if len(json_data["entry"]) > 0:
-                if "changes" in json_data["entry"][0]:
-                    if len(json_data["entry"][0]["changes"]) > 0:
-                        if "value" in json_data["entry"][0]["changes"][0]:
-                            if "messages" in json_data["entry"][0]["changes"][0]["value"]:
-                                return True
-                            else:
-                                return False
-                        else:
-                            return False
-                    else:
-                        return False
-                else:
-                    return False
-            else:
-                return False
-        else:
+        # json_data = json.loads(request.data)
+        # if "entry" in json_data:
+        #     if len(json_data["entry"]) > 0:
+        #         if "changes" in json_data["entry"][0]:
+        #             if len(json_data["entry"][0]["changes"]) > 0:
+        #                 if "value" in json_data["entry"][0]["changes"][0]:
+        #                     if "messages" in json_data["entry"][0]["changes"][0]["value"]:
+        #                         return True
+        #                     else:
+        #                         return False
+        #                 else:
+        #                     return False
+        #             else:
+        #                 return False
+        #         else:
+        #             return False
+        #     else:
+        #         return False
+        # else:
+        #     return False
+        try:
+            json_data = json.loads(request.data)
+            return json_data["entry"][0]["changes"][0]["value"]["messages"]
+        except Exception:
             return False
