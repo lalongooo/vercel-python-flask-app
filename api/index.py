c473bfa1e2e7220e3453a9fb1b0f178aab13b865
Jorge E. Hernández <hdez.jeduardo@gmail.com>
2023-08-13 01:20:46 -0600

    Troubleshoot interactive reply

--- api/index.py
+++ api/index.py
@@ -133,12 +133,12 @@ def whatsapp():
             response.status_code = 200
             return response
 
-def handle_interactive_list_reply(request):
-    # reply_content = Util.get_interactive_reply(request)
-    # print(reply_content)
-    response = make_response("reply_content")
-    response.status_code = 200
-    return response
+# def handle_interactive_list_reply(request):
+#     # reply_content = Util.get_interactive_reply(request)
+#     # print(reply_content)
+#     response = make_response("reply_content")
+#     response.status_code = 200
+#     return response
 
 def reply_with_interactive_message(to_author):
     url = GRAPH_FACEBOOK_WHATSAPP_MESSAGES_URL
